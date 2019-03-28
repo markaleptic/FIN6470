27cf793b9befeeac7e15461a665fafb4af9c018f
Mark Allred <markjallred@gmail.com>
2019-03-28 13:49:40 -0600

    Initial commit

---
+++ midterm/midterm.py
@@ -0,0 +1,15 @@
+#########################
+#                       #
+#   Mark Allred         #
+#   A01647260           #
+#   Module: midterm.py  # 
+#                       #
+#########################
+
+
+def problem1():
+    print("Hello world")
+
+
+if __name__ == "__main__":
+    problem1()
